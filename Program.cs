9cd180c6da1d06c45f2d31d1fc4c8f15f57222a7
Shane Hurtado <mhurtado1@berklee.edu>
2023-02-23 11:17:41 -0600

    changes

--- Program.cs
+++ Program.cs
@@ -182,15 +182,38 @@ void Magic()
         Console.Write($"{question}? : ");
         string answer = Console.ReadLine().ToLower();
 
-        while (answer != null)
+        while (answer == "")
         {
             Console.Write($"{question}? : ");
             answer = Console.ReadLine().ToLower();
         }
 
-        if (answer == "y")
+        if (answer != null)
         {
-            return true;
+            List<string> phrases = new List<string>() {
+            "As I see it, yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don’t count on it.",
+            "It is certain.",
+            "It is decidedly so.",
+            "Most likely.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Outlook good.",
+            "Reply hazy, try again.",
+            "Signs point to yes.",
+            "Very doubtful.",
+            "Without a doubt.",
+            "Yes.",
+            "Yes – definitely.",
+            "You may rely on it."
+        };
+            Random phrases = new Random();
+            string genRand = phrases.Next(1, 20);
         }
         else
         {
@@ -204,25 +227,25 @@ void Magic()
 }
 
 
-// List<string> words = new List<string>() { 
-// "As I see it, yes."
-// "Ask again later."
-// "Better not tell you now."
-// "Cannot predict now."
-// "Concentrate and ask again."
-// "Don’t count on it."
-// "It is certain."
-// "It is decidedly so."
-// "Most likely."
-// "My reply is no."
-// "My sources say no."
-// "Outlook not so good."
-// "Outlook good."
-// "Reply hazy, try again."
-// "Signs point to yes."
-// "Very doubtful."
-// "Without a doubt."
-// "Yes."
-// "Yes – definitely."
-// "You may rely on it."
+//     List<string> phrases = new List<string>() {
+//     "As I see it, yes.",
+//     "Ask again later.",
+//     "Better not tell you now.",
+//     "Cannot predict now.",
+//     "Concentrate and ask again.",
+//     "Don’t count on it.",
+//     "It is certain.",
+//     "It is decidedly so.",
+//     "Most likely.",
+//     "My reply is no.",
+//     "My sources say no.",
+//     "Outlook not so good.",
+//     "Outlook good.",
+//     "Reply hazy, try again.",
+//     "Signs point to yes.",
+//     "Very doubtful.",
+//     "Without a doubt.",
+//     "Yes.",
+//     "Yes – definitely.",
+//     "You may rely on it."
 // };
